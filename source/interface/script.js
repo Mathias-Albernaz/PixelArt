75cc31b1470541745e0a7fedb5f87b2beef98e4d
Mathias-Albernaz <mathiasmatu15@gmail.com>
2023-12-15 01:03:56 -0300

    Se implementa funcionalidad de elegir tamaño y boton de borrar

--- source/interface/script.js
+++ source/interface/script.js
@@ -1,8 +1,54 @@
+//*incializado de variables y elementos
 let contenedor = document.querySelector(".contenedor_principal");
+let etiquetaTamanio = document.querySelector("#etiqueta_tamanio");
+let botonBorrar = document.querySelector(".borrador");
+var click = false;
+var color = "grey";
+var slider = document.querySelector("#slider_pixeles")
+var tamanio = 32;
+etiquetaTamanio.textContent = tamanio + " x " + tamanio;
+crearPixeles(tamanio);
+botonBorrar.addEventListener('click', ()=>{
+    contenedor.innerHTML = '';
+    crearPixeles(tamanio);
+})
 
-for(i = 0; i <= (16*16); i++){
-    let pixel = document.createElement('div');
-    pixel.className = "pixel";
-    contenedor.append(pixel);
 
+//*Propiedad de clickear 
+document.body.onmousedown = () => (click = true);
+document.body.onmouseup = () => (click = false);
+
+//* Actualiza el valor del slider
+slider.oninput = function() {
+  tamanio = this.value;
+  etiquetaTamanio.textContent = tamanio + " x " + tamanio;
+  crearPixeles(tamanio);
 }
+
+//*Crea los divs
+function crearPixeles(tamanio){
+    contenedor.innerHTML = '';
+    for(i = 0; i <= (tamanio*tamanio); i++){
+        var pixel = document.createElement('div');
+        pixel.className = "pixel";
+        let tamanioPixel = 100/tamanio;
+        pixel.style.width = tamanioPixel +"%";
+        pixel.style.height = tamanioPixel +"%";
+        listeners(pixel);
+        contenedor.append(pixel);
+    }
+}
+
+
+//*Añade listeners a los pixeles
+function listeners(elemento){
+    elemento.addEventListener('mouseover', function(){
+        if(click){
+            elemento.style.backgroundColor = "grey";
+        }
+    })
+    elemento.addEventListener('click', function(){
+        elemento.style.backgroundColor = "grey";
+    })
+}
+
